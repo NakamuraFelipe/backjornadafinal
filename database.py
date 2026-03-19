c75d4b7209f58f3e760cf46ce5ff3a8099d60526
Felipe <felipenakamura29.com>
2026-03-18 22:28:19 -0300

    database

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
+import psycopg
+from psycopg.rows import dict_row
+
 def get_db_connection():
     conn = psycopg.connect(
-        "postgresql://postgres:UTbtJ7rkB1gPYE91@db.xzzgjndcfwomumrmcumo.supabase.co:5432/postgres",
+        "postgresql://postgres:UTbtJ7rkB1gPYE91@db.xzzgjndcfwomumrmcumo.supabase.co:5432/postgres?sslmode=require",
         row_factory=dict_row
     )
     return conn
